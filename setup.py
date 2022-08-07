08e3be87f7d04d280b7fe4a1193d75797c74b8cd
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2022-08-08 07:20:12 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="zotero-notion-sync",
-    version="0.1",
+    version="0.1.1",
     packages=["zotero_notion_sync"],
     install_requires=required,
     entry_points={
